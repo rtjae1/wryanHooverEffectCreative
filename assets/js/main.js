da0d4b1d3c818a7f3dd3f279251f28d2d3cf9abc
rtjae1 <ryan.t.jae@gmail.com>
2020-02-25 12:41:00 -0500

    update to start

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.color = "#444444";
+    document.getElementById("myNav").style.background = "#ffdd67";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
